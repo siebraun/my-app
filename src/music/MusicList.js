9aa4117cc88d25187b543f4e1090d4237fa1e48a
Braun, Sierra <siebraun@iu.edu>
2022-11-08 14:03:33 -0500

    removed unsued imports

--- src/music/MusicList.js
+++ src/music/MusicList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import "./music.css";
 
 export default function MusicList(props) {
--- src/music/MusicPlayer.js
+++ src/music/MusicPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect} from "react";
 import MusicList from "./MusicList";
 import PlayWidget from "./PlayWidget";
 import MusicVis from "./MusicVis";
