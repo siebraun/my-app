8fc73e0e4b6be2fd15da8ad34c1925f8c6587bc1
Braun, Sierra <siebraun@iu.edu>
2022-11-10 13:40:15 -0500

    React. I will kill you.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = document.getElementById("root");
 render(<App />, root);
